21ded47046b23f4132bb0c3b6a8e89b802eae05d
hdezfdezsh <43962864+hdezfdezsh@users.noreply.github.com>
2018-12-18 09:17:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+    <div class="container">
+            <div class="row">
+                <h3>Index</h3>
+            </div>
+             <form class="form-inline" action="buscar.php" method="get">
+                <input class="form-control mr-sm-2" type="search" name="s" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            <div class="row">
+                <p>
+                    <a href="showgame.php" class="btn ">Juegos</a>
+                    </br>
+                    </br>
+                    <a href="showuser.php" class="btn ">Usuarios</a>
+                </p>
+			</div>
+	</div>
+</body>
+</html>
